37db19120bb67fa4b7679c2c975a1ff82a1933a4
Maxy <maxicrg.05@gmail.com>
2026-07-29 00:21:40 -0600

    Empezando la Aplicación Consumidora

---
+++ src/main/java/com/aos/consumidoras/cajero_red/CajeroRedApplication.java
@@ -0,0 +1,13 @@
+package com.aos.consumidoras.cajero_red;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CajeroRedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CajeroRedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aos/consumidoras/cajero_red/CajeroRedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aos.consumidoras.cajero_red;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CajeroRedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
